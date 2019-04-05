18244dbc3211fa58faf0e0c70050ccc3436461ca
AndreGuima <andrepaivaguimaraes@gmail.com>
2019-04-04 22:52:58 -0300

    add update, delete and add method

--- client/src/app/shared/aircraft/aircraft.service.ts
+++ client/src/app/shared/aircraft/aircraft.service.ts
@@ -4,11 +4,31 @@ import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AircraftService {
+  public API = '//localhost:8080';
+  public AIRCRAFT_API = this.API + '/aircrafts';
 
   constructor(private http: HttpClient) { 
   }
 
   getAll(): Observable<any> {
-    return this.http.get('//localhost:8080/aircrafts');
+    return this.http.get(this.AIRCRAFT_API);
+  }
+
+  get(id: string) {
+    return this.http.get(this.AIRCRAFT_API + '/' + id);
+  }
+
+  save(aircraft: any): Observable<any> {
+    let result: Observable<Object>;
+    if (aircraft['href']) {
+      result = this.http.put(aircraft.href, aircraft);
+    } else {
+      result = this.http.post(this.AIRCRAFT_API, aircraft);
+    }
+    return result;
+  }
+
+  remove(href: string) {
+    return this.http.delete(href);
   }
 }
